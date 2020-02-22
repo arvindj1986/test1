d9a07562c90ca4a2c235d235cb258143f054131b
arvindj1986 <arvindj1986@gmail.com>
2020-02-22 06:01:52 -0500

    firsttest

---
+++ calc.c
@@ -0,0 +1,10 @@
+int main() {
+    char operator;
+    double first, second;
+    printf("Enter an operator (+, -, *,): ");
+    scanf("%c", &operator);
+    printf("Enter two operands: ");
+    scanf("%lf %lf", &first, &second);
+    printf("Adition:%d",first+second);
+    return 0;
+}
